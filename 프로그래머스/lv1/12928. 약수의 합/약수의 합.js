444b0e01b36638143305a5b88dca57394e0416d2
Jeong sun woo <52305780+Jeongsunwoo@users.noreply.github.com>
2023-07-11 22:59:59 +0900

    [level 1] Title: 약수의 합, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let answer = 0;
+    for(i=0;i<=n;i++){
+        if(n%i == 0){ // n의 약수 찾기
+           answer += i; //찾은 약수를 answer에 더함
+        }
+    }
+    return answer;
+}
