5b1131a94192bc37bc60ba93c544b68a9cbefd63
Jeong sun woo <52305780+Jeongsunwoo@users.noreply.github.com>
2023-08-08 21:44:01 +0900

    [unrated] Title: 카운트 다운, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.js
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.js
@@ -1,7 +1,4 @@
 function solution(start, end) {
-    let answer = [];
-    for(let i = start; i >= end; i--){
-        answer.push(i)
-    }
-    return answer
+    var answer = [];
+    return answer;
 }
